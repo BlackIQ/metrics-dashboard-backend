50c53ea98cc909062ce19ea985d5ea97906bbcdc
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2024-11-21 19:16:33 +0330

    Update

--- app/middlewares/index.js
+++ app/middlewares/index.js
@@ -1,5 +1,11 @@
-import log from "$app/middlewares/log/log.middleware.js";
+// ----------------------------------------------
+// $app/middlewares
+// index.js
+// ----------------------------------------------
+// Exporting all API middlewares.
+// Like configs, create them in directories and export them here.
+
 import jwt from "$app/middlewares/jwt/jwt.middleware.js";
 import key from "$app/middlewares/key/key.middleware.js";
 
-export { log, key, jwt };
+export { key, jwt };
