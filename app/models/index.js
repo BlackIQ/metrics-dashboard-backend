ba7e8da37ab7ca1367eec943381608dedc4e2bdc
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2024-11-21 19:46:34 +0330

    Ok

---
+++ app/models/index.js
@@ -0,0 +1,12 @@
+// ----------------------------------------------
+// $app/models
+// index.js
+// ----------------------------------------------
+// Exporting all databse models.
+// Like Role, User and etc...
+
+import Role from "$app/models/role/role.model.js";
+import User from "$app/models/user/user.model.js";
+import Permission from "$app/models/permission/permission.model.js";
+
+export { Role, User, Permission };
