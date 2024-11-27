33d866fc67658441bfef263c5158ce11418b56ab
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2024-11-27 18:54:31 +0330

    Add config of firebase

---
+++ app/config/firebase/firebase.config.js
@@ -0,0 +1,20 @@
+// ----------------------------------------------
+// $app/config/firebase
+// firebase.config.js
+// ----------------------------------------------
+// Firebase configurations.
+// Here we export Firebase data.
+
+import env from "$app/env/index.js";
+
+export default {
+  firebaseConfig: {
+    apiKey: "AIzaSyBfmrqA9sHlC4HIojeWM4uF8rRORQ2vGOU",
+    authDomain: "amce-monitoring.firebaseapp.com",
+    projectId: "amce-monitoring",
+    storageBucket: "amce-monitoring.firebasestorage.app",
+    messagingSenderId: "898941321201",
+    appId: "1:898941321201:web:bab27bafe2edd2c8518e50",
+    measurementId: "G-KJ82QNS39L",
+  },
+};
--- app/config/index.js
+++ app/config/index.js
@@ -8,5 +8,6 @@
 import appConfig from "$app/config/app/app.config.js";
 import fileConfig from "$app/config/file/file.config.js";
 import databaseConfig from "$app/config/database/database.config.js";
+import firebaseConfig from "$app/config/firebase/firebase.config.js";
 
-export { appConfig, fileConfig, databaseConfig };
+export { appConfig, fileConfig, databaseConfig, firebaseConfig };
