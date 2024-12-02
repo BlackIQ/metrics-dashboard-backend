c3083c3a7be3117fe1058ba92bdabc5f48688b43
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2024-12-02 12:04:46 +0330

    Fuck Firebase

--- app/instances/index.js
+++ app/instances/index.js
@@ -1,12 +1,10 @@
 // ----------------------------------------------
-// $app/connections
+// $app/instances
 // index.js
 // ----------------------------------------------
-// Exporting all databse connections.
-// Like configs, create them in directories and export them here.
+// Exporting all instances.
+// Like Firebase.
 
-import redis from "$app/connections/redis/redis.connection.js";
-import mongo from "$app/connections/mongo/mongo.connection.js";
-import influx from "$app/connections/influx/influx.connection.js";
+const a = "instance";
 
-export { redis, mongo, influx };
+export { a };
