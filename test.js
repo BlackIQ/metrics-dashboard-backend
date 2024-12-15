0239ed9056879339a1dcc11e0ca24050be63316a
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2024-12-15 20:49:49 +0330

    Start a init

--- test.js
+++ test.js
@@ -1,9 +1,11 @@
 import { appConfig } from "$app/config/index.js";
+import { startMetricsCollection } from "$app/services/index.js";
 
 import app from "$app";
 
 import chalk from "chalk";
 
-app.listen(appConfig.port, () =>
-  console.log(chalk.cyan(`App is running on ${appConfig.port}`))
-);
+app.listen(appConfig.port, () => {
+  console.log(chalk.cyan(`App is running on ${appConfig.port}`));
+  startMetricsCollection();
+});
