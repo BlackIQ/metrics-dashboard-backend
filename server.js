5b3be61f23f66f0f4c09108a6b5a4c9631e318f0
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2024-12-16 13:29:31 +0330

    Updating runners

--- server.js
+++ server.js
@@ -3,42 +3,42 @@ import chalk from "chalk";
 import os from "os";
 
 import { appConfig } from "$app/config/index.js";
+import { startMetricsCollection } from "$app/services/index.js";
+
 import app from "$app";
 
 const numCPUs = os.cpus().length;
 
-console.log(`----- Number of cors: ${numCPUs} -----`);
+console.log(`----- Number of cores: ${numCPUs} -----`);
 
 if (cluster.isMaster) {
   console.log(`Master ${process.pid} is running`);
 
+  // Fork workers for app servers
   for (let i = 0; i < numCPUs; i++) {
     cluster.fork();
   }
 
+  // Fork a dedicated worker for metrics collection
+  const metricsWorker = cluster.fork({ METRICS_WORKER: "true" });
+  metricsWorker.on("online", () => {
+    console.log(`Metrics worker ${metricsWorker.process.pid} is online`);
+  });
+
   cluster.on("exit", (worker, code, signal) => {
-    console.log(`Worker ${worker.process.pid} died`);
+    console.log(
+      `Worker ${worker.process.pid} exited with code ${code} and signal ${signal}`
+    );
   });
 } else {
-  app.listen(appConfig.port, () =>
-    console.log(chalk.cyan(`App is running on ${appConfig.port}`))
-  );
+  // Metrics worker process
+  if (process.env.METRICS_WORKER === "true") {
+    console.log(chalk.yellow(`Metrics worker ${process.pid} started`));
+    startMetricsCollection(); // Only runs in this worker
+  } else {
+    // Regular app worker process
+    app.listen(appConfig.port, () => {
+      console.log(chalk.cyan(`App is running on ${appConfig.port}`));
+    });
+  }
 }
-
-cluster.on("fork", (worker) => {
-  console.log(`Worker ${worker.process.pid} has been forked`);
-});
-
-cluster.on("online", (worker) => {
-  console.log(`Worker ${worker.process.pid} is online`);
-});
-
-cluster.on("disconnect", (worker) => {
-  console.log(`Worker ${worker.process.pid} has disconnected`);
-});
-
-cluster.on("exit", (worker, code, signal) => {
-  console.log(
-    `Worker ${worker.process.pid} exited with code ${code} and signal ${signal}`
-  );
-});
