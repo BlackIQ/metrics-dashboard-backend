69bb960d9737efd1eb693e48672abe43d7e7afd1
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2024-12-19 21:51:13 +0330

    Remove stuff

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ -z "$1" ]; then
-  echo "Usage: $0 <tag>"
-  exit 1
-fi
-
-TAG=$1
-docker run --network monitoring-network -p 9999:8000 --name monitoring-api --detach monitoring-api:$TAG
