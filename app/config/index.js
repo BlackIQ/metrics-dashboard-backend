3332ad46de5d35c148d58111174c501be7426a3a
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2024-12-24 13:10:36 +0330

    Remove firebase stuff

--- app/config/index.js
+++ app/config/index.js
@@ -8,6 +8,5 @@
 import appConfig from "$app/config/app/app.config.js";
 import fileConfig from "$app/config/file/file.config.js";
 import databaseConfig from "$app/config/database/database.config.js";
-import firebaseConfig from "$app/config/firebase/firebase.config.js";
 
-export { appConfig, fileConfig, databaseConfig, firebaseConfig };
+export { appConfig, fileConfig, databaseConfig };
