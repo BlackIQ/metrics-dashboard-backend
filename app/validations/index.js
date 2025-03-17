36c81d0815550c138e4fdf632cc46d6af10fc11d
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2025-03-17 20:04:28 +0330

    Export Tag, Role and Permission

--- app/validations/index.js
+++ app/validations/index.js
@@ -1,1 +1,4 @@
 export * from "$app/validations/group/group.validations.js";
+export * from "$app/validations/permission/permission.validations.js";
+export * from "$app/validations/role/role.validations.js";
+export * from "$app/validations/tag/tag.validations.js";
