e0a652bdbc6c2efb2bf0a259f7d6e80b7dea9c45
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2025-03-18 01:46:07 +0330

    Put the createToekn there in utils

--- app/utils/index.js
+++ app/utils/index.js
@@ -1,5 +1,6 @@
 import { sendTelegramMessage } from "$app/utils/send_telegram/send_telegram.util.js";
 import { sendEmail } from "$app/utils/send_email/send_email.util.js";
 import { generateSecureValue } from "$app/utils/generator/generator.util.js";
+import { createToken } from "$app/utils/token/token.util.js";
 
-export { sendTelegramMessage, sendEmail, generateSecureValue };
+export { sendTelegramMessage, sendEmail, generateSecureValue, createToken };
---
+++ app/utils/token/token.util.js
@@ -0,0 +1,21 @@
+// ----------------------------------------------
+// $app/functions/token
+// token.function.js
+// ----------------------------------------------
+// Token generator.
+// We use JWT for authorization. To create the token, we use this function.
+
+import { appConfig } from "$app/config/index.js";
+
+import jwt from "jsonwebtoken";
+
+export const createToken = (payload) => {
+  // ----------------------------------------------
+  // createToken()
+  // ----------------------------------------------
+  // Inputs are just the user id. The data is the id.
+  // Find secret in appConfig.
+  // Then set expiresIn for 7 days.
+
+  return jwt.sign(payload, appConfig.secret, { expiresIn: "30d" });
+};
