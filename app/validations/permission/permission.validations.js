7a20b4145f4d2825f5abceba20aa13d0078e5014
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2025-03-17 19:57:10 +0330

    Fix name

--- app/validations/permission/permission.validations.js
+++ app/validations/permission/permission.validations.js
@@ -8,10 +8,10 @@ export const permissionSchema = Joi.object({
     "any.required": "Label is required",
   }),
   value: Joi.string().min(1).max(100).required().messages({
-    "string.empty": "Label cannot be empty",
-    "string.min": "Label must be at least 1 character",
-    "string.max": "Label cannot exceed 100 characters",
-    "any.required": "Label is required",
+    "string.empty": "Value cannot be empty",
+    "string.min": "Value must be at least 1 character",
+    "string.max": "Value cannot exceed 100 characters",
+    "any.required": "Value is required",
   }),
 });
 
