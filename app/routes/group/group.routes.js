169958468cb1cd883b7674185bb9b9ff25569113
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2025-03-17 16:39:26 +0330

    First validate, then pass to next

--- app/routes/group/group.routes.js
+++ app/routes/group/group.routes.js
@@ -8,19 +8,33 @@
 import express from "express";
 
 import { Group } from "$app/controllers/index.js";
-import { groupSchema, groupUpdateSchema } from "$app/validations/index.js";
+import {
+  groupSchema,
+  groupUpdateSchema,
+  groupParamsSchema,
+} from "$app/validations/index.js";
 import { resourceOwnership, validate } from "$app/middlewares/index.js";
 
 const router = express.Router();
 
 router.get("/", Group.ALL);
-router.post("/", validate(groupSchema), Group.CREATE);
-router.get("/:id", resourceOwnership("Group"), Group.SINGLE);
-router.delete("/:id", resourceOwnership("Group"), Group.DELETE);
+router.post("/", validate({ bodySchema: groupSchema }), Group.CREATE);
+router.get(
+  "/:id",
+  validate({ paramsSchema: groupParamsSchema }),
+  resourceOwnership("Group"),
+  Group.SINGLE
+);
+router.delete(
+  "/:id",
+  validate({ paramsSchema: groupParamsSchema }),
+  resourceOwnership("Group"),
+  Group.DELETE
+);
 router.patch(
   "/:id",
+  validate({ bodySchema: groupUpdateSchema, paramsSchema: groupParamsSchema }),
   resourceOwnership("Group"),
-  validate(groupUpdateSchema),
   Group.UPDATE
 );
 
