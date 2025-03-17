729d341e139cb641d7ad84231d1f7ca71d6b996f
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2025-03-17 16:39:11 +0330

    Now having params and body alongside eachother

--- app/middlewares/validate/validate.middleware.js
+++ app/middlewares/validate/validate.middleware.js
@@ -1,14 +1,33 @@
-const validate = (schema) => (req, res, next) => {
-  const { error } = schema.validate(req.body, { abortEarly: false });
+import Joi from "joi";
 
-  if (error) {
-    return res.status(400).json({
-      message: "Validation failed",
-      errors: error.details.map((d) => d.message),
-    });
-  }
+const validate =
+  ({ bodySchema, paramsSchema }) =>
+  (req, res, next) => {
+    if (bodySchema) {
+      const { error: bodyError } = bodySchema.validate(req.body, {
+        abortEarly: false,
+      });
+      if (bodyError) {
+        return res.status(400).json({
+          message: "Body validation failed",
+          errors: bodyError.details.map((d) => d.message),
+        });
+      }
+    }
 
-  next();
-};
+    if (paramsSchema) {
+      const { error: paramsError } = paramsSchema.validate(req.params, {
+        abortEarly: false,
+      });
+      if (paramsError) {
+        return res.status(400).json({
+          message: "Params validation failed",
+          errors: paramsError.details.map((d) => d.message),
+        });
+      }
+    }
+
+    next();
+  };
 
 export default validate;
