62cab580ad4c9fef7721168046b7b91832d11e0f
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2025-03-18 02:19:25 +0330

    Add rateLimit middleware

--- app/middlewares/index.js
+++ app/middlewares/index.js
@@ -7,6 +7,7 @@
 
 import jwt from "$app/middlewares/jwt/jwt.middleware.js";
 import key from "$app/middlewares/key/key.middleware.js";
+import ratelimit from "$app/middlewares/ratelimit/ratelimit.middleware.js";
 import validate from "$app/middlewares/validate/validate.middleware.js";
 import superuser from "$app/middlewares/superuser/superuser.middleware.js";
 import {
@@ -14,4 +15,12 @@ import {
   userOwnership,
 } from "$app/middlewares/ownership/ownership.middleware.js";
 
-export { key, jwt, resourceOwnership, userOwnership, superuser, validate };
+export {
+  key,
+  jwt,
+  resourceOwnership,
+  userOwnership,
+  superuser,
+  validate,
+  ratelimit,
+};
