458b438e5c797fccbd0b209690997a0c7f153541
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2025-03-17 20:36:22 +0330

    Validation implementation in routes | Alert

--- app/routes/alert/alert.routes.js
+++ app/routes/alert/alert.routes.js
@@ -8,15 +8,29 @@
 import express from "express";
 
 import { Alert } from "$app/controllers/index.js";
-
-import { resourceOwnership } from "$app/middlewares/index.js";
+import {
+  alertSchema,
+  alertUpdateSchema,
+  alertParamsSchema,
+} from "$app/validations/index.js";
+import { resourceOwnership, validate } from "$app/middlewares/index.js";
 
 const router = express.Router();
 
 router.get("/", Alert.ALL);
-router.post("/", Alert.CREATE);
-router.patch("/:id", resourceOwnership("Alert"), Alert.UPDATE);
-router.delete("/:id", resourceOwnership("Alert"), Alert.DELETE);
-router.post("/test", Alert.TEST_ALERT);
+router.post("/", validate({ bodySchema: alertSchema }), Alert.CREATE);
+router.patch(
+  "/:id",
+  validate({ bodySchema: alertUpdateSchema, paramsSchema: alertParamsSchema }),
+  resourceOwnership("Alert"),
+  Alert.UPDATE
+);
+router.delete(
+  "/:id",
+  validate({ paramsSchema: alertParamsSchema }),
+  resourceOwnership("Alert"),
+  Alert.DELETE
+);
+router.post("/test", Alert.TEST_ALERT); // TODO: Validation for TEST
 
 export default router;
