b69745f0baec83503ce9021814f7b5127d1e0f58
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2025-03-17 20:04:43 +0330

    Validation for Permission

--- app/routes/permission/permission.routes.js
+++ app/routes/permission/permission.routes.js
@@ -8,17 +8,36 @@
 import express from "express";
 
 import { Permission } from "$app/controllers/index.js";
-
-import { superuser } from "$app/middlewares/index.js";
+import {
+  permissionSchema,
+  permissionUpdateSchema,
+  permissionParamsSchema,
+} from "$app/validations/index.js";
+import { superuser, validate } from "$app/middlewares/index.js";
 
 const router = express.Router();
 
 router.use(superuser);
 
 router.get("/", Permission.ALL);
-router.post("/", Permission.CREATE);
-router.get("/:id", Permission.SINGLE);
-router.delete("/:id", Permission.DELETE);
-router.patch("/:id", Permission.UPDATE);
+router.post("/", validate({ bodySchema: permissionSchema }), Permission.CREATE);
+router.get(
+  "/:id",
+  validate({ paramsSchema: permissionParamsSchema }),
+  Permission.SINGLE
+);
+router.delete(
+  "/:id",
+  validate({ paramsSchema: permissionParamsSchema }),
+  Permission.DELETE
+);
+router.patch(
+  "/:id",
+  validate({
+    bodySchema: permissionUpdateSchema,
+    paramsSchema: permissionParamsSchema,
+  }),
+  Permission.UPDATE
+);
 
 export default router;
