6a1d2d1379f8e9fdc05222a933077452e2ee5d02
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2025-03-17 20:36:28 +0330

    Validation implementation in routes | User

--- app/routes/user/user.routes.js
+++ app/routes/user/user.routes.js
@@ -8,15 +8,39 @@
 import express from "express";
 
 import { User } from "$app/controllers/index.js";
-
-import { userOwnership } from "$app/middlewares/index.js";
+import {
+  userUpdateSchema,
+  userParamsSchema,
+  userPasswordSchema,
+} from "$app/validations/index.js";
+import { userOwnership, validate } from "$app/middlewares/index.js";
 
 const router = express.Router();
 
 router.get("/", User.ALL);
-router.get("/:id", userOwnership, User.SINGLE);
-router.delete("/:id", userOwnership, User.DELETE);
-router.patch("/:id", userOwnership, User.UPDATE);
-router.patch("/password/:id", userOwnership, User.CHANGE_PASSWORD);
+router.get(
+  "/:id",
+  validate({ paramsSchema: userParamsSchema }),
+  userOwnership,
+  User.SINGLE
+);
+router.delete(
+  "/:id",
+  validate({ paramsSchema: userParamsSchema }),
+  userOwnership,
+  User.DELETE
+);
+router.patch(
+  "/:id",
+  validate({ bodySchema: userUpdateSchema, paramsSchema: userParamsSchema }),
+  userOwnership,
+  User.UPDATE
+);
+router.patch(
+  "/password/:id",
+  validate({ bodySchema: userPasswordSchema, paramsSchema: userParamsSchema }),
+  userOwnership,
+  User.CHANGE_PASSWORD
+);
 
 export default router;
