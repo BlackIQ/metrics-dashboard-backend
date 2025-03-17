e335d4ff938c70143933546c0c130fbe6a54d106
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2025-03-17 20:05:10 +0330

    Validation for Tag

--- app/routes/tag/tag.routes.js
+++ app/routes/tag/tag.routes.js
@@ -8,15 +8,34 @@
 import express from "express";
 
 import { Tag } from "$app/controllers/index.js";
-
-import { resourceOwnership } from "$app/middlewares/index.js";
+import {
+  tagSchema,
+  tagUpdateSchema,
+  tagParamsSchema,
+} from "$app/validations/index.js";
+import { resourceOwnership, validate } from "$app/middlewares/index.js";
 
 const router = express.Router();
 
 router.get("/", Tag.ALL);
-router.post("/", Tag.CREATE);
-router.get("/:id", resourceOwnership("Tag"), Tag.SINGLE);
-router.delete("/:id", resourceOwnership("Tag"), Tag.DELETE);
-router.patch("/:id", resourceOwnership("Tag"), Tag.UPDATE);
+router.post("/", validate({ bodySchema: tagSchema }), Tag.CREATE);
+router.get(
+  "/:id",
+  validate({ paramsSchema: tagParamsSchema }),
+  resourceOwnership("Tag"),
+  Tag.SINGLE
+);
+router.delete(
+  "/:id",
+  validate({ paramsSchema: tagParamsSchema }),
+  resourceOwnership("Tag"),
+  Tag.DELETE
+);
+router.patch(
+  "/:id",
+  validate({ bodySchema: tagUpdateSchema, paramsSchema: tagParamsSchema }),
+  resourceOwnership("Tag"),
+  Tag.UPDATE
+);
 
 export default router;
