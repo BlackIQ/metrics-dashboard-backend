add130ba0ee81ab8e55706215de7c096854b3c82
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2025-03-17 14:50:17 +0330

    Base of logger. Using Winston

---
+++ app/log/index.js
@@ -0,0 +1,47 @@
+// Winston
+import winston from "winston";
+import { MongoDB } from "winston-mongodb";
+
+// Stuff
+import path from "path";
+
+// Database Log
+import { logMongo } from "$app/connections/index.js";
+
+// File log
+import { fileConfig } from "$app/config/index.js";
+
+console.log(fileConfig);
+
+const logger = winston.createLogger({
+  level: "info",
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.json()
+  ),
+  transports: [
+    new MongoDB({
+      db: logMongo,
+      collection: "logs",
+      level: "info",
+      storeHost: true,
+      capped: true,
+      cappedSize: 10000000,
+      tryReconnect: true,
+    }),
+    new winston.transports.File({
+      filename: path.join(fileConfig.filePath, "app.log"),
+      level: "info",
+    }),
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.simple()
+      ),
+    }),
+  ],
+});
+
+logger.info("Logging initialized", { context: "startup" });
+
+export default logger;
