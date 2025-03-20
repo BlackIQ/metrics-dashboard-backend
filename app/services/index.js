7da5fbfa59532e47c612824e91bf30077eda30f5
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2025-03-20 19:06:55 +0330

    Update

--- app/services/index.js
+++ app/services/index.js
@@ -1,5 +1,5 @@
 import { pullService } from "$app/services/pull/pull.service.js";
 import { triggerService } from "$app/services/trigger/trigger.service.js";
-import { startEventDriven } from "$app/services/init/init.service.js";
+import { startEventDriven } from "$app/services/_init/_init.service.js";
 
-export { pullService, triggerService, startEventDriven };
+export { startEventDriven, pullService, triggerService };
