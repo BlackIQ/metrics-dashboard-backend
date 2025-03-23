21cde14207bb5f9bcc74f28423bbf561eb986495
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2025-03-23 20:17:42 +0330

    Just ordering some routes

--- app/routes/auth/oauth.routes.js
+++ app/routes/auth/oauth.routes.js
@@ -5,8 +5,8 @@ import { OAuth } from "$app/controllers/index.js";
 const router = express.Router();
 
 router.post("/google", OAuth.GOOGLE_LOGIN);
+router.post("/github", OAuth.GITHUB_LOGIN);
 router.post("/facebook", OAuth.FACEBOOK_LOGIN);
 router.post("/microsoft", OAuth.MICROSOFT_LOGIN);
-router.post("/github", OAuth.GITHUB_LOGIN);
 
 export default router;
