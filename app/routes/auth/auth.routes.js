2a96bd130e56277ca86fa1ee8a9289c51b0e1f72
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2025-03-23 13:30:25 +0330

    Add forgot-password routes

--- app/routes/auth/auth.routes.js
+++ app/routes/auth/auth.routes.js
@@ -13,17 +13,27 @@ import {
   authRegisterSchema,
   authConfirmSchema,
   authResendConfirmSchema,
+  authChangeEmailSchema,
+  authConfirmEmailChangeSchema,
+  authForgotPasswordSchema,
+  authResetPasswordSchema,
 } from "$app/validations/index.js";
-import { validate } from "$app/middlewares/index.js";
+import { validate, jwt } from "$app/middlewares/index.js";
 
 const router = express.Router();
 
+// Authentocation
 router.post("/login", validate({ bodySchema: authLoginSchema }), Auth.LOGIN);
 router.post(
   "/register",
   validate({ bodySchema: authRegisterSchema }),
   Auth.REGISTER
 );
+
+// Logout
+router.post("/logout", Auth.LOGOUT);
+
+// Confirm Account
 router.post(
   "/confirm",
   validate({ bodySchema: authConfirmSchema }),
@@ -34,6 +44,30 @@ router.post(
   validate({ bodySchema: authResendConfirmSchema }),
   Auth.RESEND_CONFIRM
 );
-router.post("/logout", Auth.LOGOUT);
+
+// Change Email
+router.post(
+  "/change-email",
+  jwt,
+  validate({ bodySchema: authChangeEmailSchema }),
+  Auth.CHANGE_EMAIL
+);
+router.post(
+  "/confirm-email-change",
+  validate({ bodySchema: authConfirmEmailChangeSchema }),
+  Auth.CONFIRM_EMAIL_CHANGE
+);
+
+// Reset Password
+router.post(
+  "/forgot-password",
+  validate({ bodySchema: authForgotPasswordSchema }),
+  Auth.FORGOT_PASSWORD
+);
+router.post(
+  "/reset-password",
+  validate({ bodySchema: authResetPasswordSchema }),
+  Auth.RESET_PASSWORD
+);
 
 export default router;
