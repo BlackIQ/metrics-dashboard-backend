cc72503576097eefd44c0a8cdef555fb5ea8a7a0
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2025-03-23 12:29:06 +0330

    Update generate jwt with more times based on remember parameter

--- app/utils/index.js
+++ app/utils/index.js
@@ -1,6 +1,11 @@
 import { sendTelegramMessage } from "$app/utils/send_telegram/send_telegram.util.js";
 import { sendEmail } from "$app/utils/send_email/send_email.util.js";
 import { generateSecureValue } from "$app/utils/generator/generator.util.js";
-import { createToken } from "$app/utils/token/token.util.js";
+import { generateAuthToken } from "$app/utils/token/token.util.js";
 
-export { sendTelegramMessage, sendEmail, generateSecureValue, createToken };
+export {
+  sendTelegramMessage,
+  sendEmail,
+  generateSecureValue,
+  generateAuthToken,
+};
--- app/utils/token/token.util.js
+++ app/utils/token/token.util.js
@@ -1,21 +1,13 @@
-// ----------------------------------------------
-// $app/functions/token
-// token.function.js
-// ----------------------------------------------
-// Token generator.
-// We use JWT for authorization. To create the token, we use this function.
-
 import { appConfig } from "$app/config/index.js";
 
 import jwt from "jsonwebtoken";
 
-export const createToken = (payload) => {
-  // ----------------------------------------------
-  // createToken()
-  // ----------------------------------------------
-  // Inputs are just the user id. The data is the id.
-  // Find secret in appConfig.
-  // Then set expiresIn for 7 days.
+export const generateAuthToken = (payload, remember = false) => {
+  if (!payload || !appConfig.secret) {
+    throw new Error("Invalid payload or secret for token generation");
+  }
 
-  return jwt.sign(payload, appConfig.secret, { expiresIn: "30d" });
+  return jwt.sign(payload, appConfig.secret, {
+    expiresIn: remember ? "30d" : "1h", // 30 days if "remember me", else 1 hour
+  });
 };
