83895f1a1e4b2283ac76a0922ed90fa0c0dd705e
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2025-03-23 20:05:32 +0330

    Firebase config

---
+++ app/config/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// ----------------------------------------------
+// $app/config/firebase
+// firebase.config.js
+// ----------------------------------------------
+// Firebase configurations.
+// Here we export firebase data.
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCcpEGZ7zXjxAE1oxXcyDA-M6xDwfeyQb0",
+  authDomain: "openhubble-cloud.firebaseapp.com",
+  projectId: "openhubble-cloud",
+  storageBucket: "openhubble-cloud.firebasestorage.app",
+  messagingSenderId: "630599906091",
+  appId: "1:630599906091:web:e157d67c4ef29e38ac0b6e",
+  measurementId: "G-SGS0TFX63V",
+};
+
+export default firebaseConfig;
