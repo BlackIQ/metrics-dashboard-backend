025584f936ea065bb344fff7f5e5e7425e551ef2
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2025-03-24 19:32:28 +0330

    Add graph routes

---
+++ app/routes/graph/graph.routes.js
@@ -0,0 +1,42 @@
+// ----------------------------------------------
+// $app/routes/graph
+// graph.routes.js
+// ----------------------------------------------
+// Graph Routes.
+// All routes of graph crud.
+
+import express from "express";
+
+import { Graph } from "$app/controllers/index.js";
+import {
+  graphSchema,
+  graphUpdateSchema,
+  graphParamsSchema,
+  paginationSchema,
+} from "$app/validations/index.js";
+import { resourceOwnership, validate } from "$app/middlewares/index.js";
+
+const router = express.Router();
+
+router.get("/", validate({ querySchema: paginationSchema }), Graph.ALL);
+router.post("/", validate({ bodySchema: graphSchema }), Graph.CREATE);
+router.get(
+  "/:id",
+  validate({ paramsSchema: graphParamsSchema }),
+  resourceOwnership("Graph"),
+  Graph.SINGLE
+);
+router.delete(
+  "/:id",
+  validate({ paramsSchema: graphParamsSchema }),
+  resourceOwnership("Graph"),
+  Graph.DELETE
+);
+router.patch(
+  "/:id",
+  validate({ bodySchema: graphUpdateSchema, paramsSchema: graphParamsSchema }),
+  resourceOwnership("Graph"),
+  Graph.UPDATE
+);
+
+export default router;
