22ae4699d905c29bb78fbb1543f8d0d5d55fd5ba
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2025-03-24 18:47:32 +0330

    Return data with most easy way!

--- app/controllers/metric/metric.controllers.js
+++ app/controllers/metric/metric.controllers.js
@@ -5,10 +5,11 @@ const { influx: influxConfig } = databaseConfig;
 
 export const READ = async (req, res) => {
   const { host } = req.params;
-  const { measurements } = req.body;
+  const { measurements, fields } = req.body;
   const { start = "-1h", end = "now()" } = req.query;
 
   let queryMeasurements = "";
+
   const countMeasurements = measurements.length;
   measurements.forEach((measurement, index) => {
     const isLast = countMeasurements === index + 1;
@@ -19,29 +20,59 @@ export const READ = async (req, res) => {
   try {
     const queryAPI = influx.getQueryApi(influxConfig.org);
 
-    const fluxQuery = `
+    let fluxQuery = `
       from(bucket: "${influxConfig.bucket}")
         |> range(start: ${start}, stop: ${end})
         |> filter(fn: (r) => 
           (${queryMeasurements}) 
           and r.host_id == "${host}"
         )
-        |> keep(columns: ["_time", "_value", "_field", "_measurement", "host_id"])
+    `;
+
+    if (fields && typeof fields === "object") {
+      let fieldFilter = "";
+
+      Object.entries(fields).forEach(([measurement, fieldList], index) => {
+        if (Array.isArray(fieldList) && fieldList.length > 0) {
+          const fieldConditions = fieldList
+            .map((field) => `r._field == "${field}"`)
+            .join(" or ");
+          fieldFilter += `(r._measurement == "${measurement}" and (${fieldConditions}))`;
+
+          if (index < Object.keys(fields).length - 1) fieldFilter += " or ";
+        }
+      });
+
+      if (fieldFilter) {
+        fluxQuery += `
+          |> filter(fn: (r) => ${fieldFilter})
+        `;
+      }
+    }
+
+    fluxQuery += `
+      |> keep(columns: ["_time", "_value", "_field", "_measurement", "host_id"])
     `;
 
     const metrics = await queryAPI.collectRows(fluxQuery);
 
-    // Group data by measurement and field for a more compact structure
     const formattedMetrics = {};
-    metrics.forEach(row => {
+
+    metrics.forEach((row) => {
       const { _measurement, _field, _time, _value } = row;
+
       if (!formattedMetrics[_measurement]) {
         formattedMetrics[_measurement] = {};
       }
+
       if (!formattedMetrics[_measurement][_field]) {
         formattedMetrics[_measurement][_field] = [];
       }
-      formattedMetrics[_measurement][_field].push({ time: _time, value: _value });
+
+      formattedMetrics[_measurement][_field].push({
+        time: _time,
+        value: _value,
+      });
     });
 
     const response = {
@@ -50,6 +81,7 @@ export const READ = async (req, res) => {
     };
 
     const responseSize = Buffer.byteLength(JSON.stringify(response), "utf8");
+
     console.log(`Response size: ${responseSize} bytes`);
 
     return res.status(200).json(response);
