688fd96b0b429226bd9ba30582837149d61cb3cf
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2025-03-24 19:32:38 +0330

    Add page routes

---
+++ app/routes/page/page.routes.js
@@ -0,0 +1,42 @@
+// ----------------------------------------------
+// $app/routes/page
+// page.routes.js
+// ----------------------------------------------
+// Page Routes.
+// All routes of page crud.
+
+import express from "express";
+
+import { Page } from "$app/controllers/index.js";
+import {
+  pageSchema,
+  pageUpdateSchema,
+  pageParamsSchema,
+  paginationSchema,
+} from "$app/validations/index.js";
+import { resourceOwnership, validate } from "$app/middlewares/index.js";
+
+const router = express.Router();
+
+router.get("/", validate({ querySchema: paginationSchema }), Page.ALL);
+router.post("/", validate({ bodySchema: pageSchema }), Page.CREATE);
+router.get(
+  "/:id",
+  validate({ paramsSchema: pageParamsSchema }),
+  resourceOwnership("Page"),
+  Page.SINGLE
+);
+router.delete(
+  "/:id",
+  validate({ paramsSchema: pageParamsSchema }),
+  resourceOwnership("Page"),
+  Page.DELETE
+);
+router.patch(
+  "/:id",
+  validate({ bodySchema: pageUpdateSchema, paramsSchema: pageParamsSchema }),
+  resourceOwnership("Page"),
+  Page.UPDATE
+);
+
+export default router;
