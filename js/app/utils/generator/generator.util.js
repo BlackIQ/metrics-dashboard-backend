9d72475ab94604e60012cf74c194c75494d6dcba
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2026-07-21 21:08:19 +0330

    Remove more stuff

--- js/app/utils/generator/generator.util.js
+++
@@ -1,8 +0,0 @@
-// Libs
-import crypto from "crypto";
-
-export const generateSecureValue = (length = 20) =>
-  crypto
-    .randomBytes(Math.ceil(length / 2))
-    .toString("hex")
-    .slice(0, length);
