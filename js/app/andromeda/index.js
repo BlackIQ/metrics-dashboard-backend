53a95e84d55cf5e9feab3a12aad3344bb662379c
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2026-07-21 21:44:13 +0330

    Totally remove js stuff

--- js/app/andromeda/index.js
+++
@@ -1,5 +0,0 @@
-import { pullService } from "$app/andromeda/pull/pull.service.js";
-import { triggerService } from "$app/andromeda/trigger/trigger.service.js";
-import { startEventDriven } from "$app/andromeda/_init/_init.service.js";
-
-export { startEventDriven, pullService, triggerService };
