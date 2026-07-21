cb4986504ba949ee0a7d12f01c80d8a1930be6fe
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2026-07-21 21:12:04 +0330

    Add router: Page

---
+++ routers/page.py
@@ -0,0 +1,135 @@
+# FastAPI
+from fastapi import APIRouter, Depends, HTTPException, status
+
+# SQLAlchemy
+from sqlalchemy.orm import Session
+
+# Datetime
+from datetime import datetime, timezone
+
+# UUID
+from uuid import UUID
+
+# Application
+from dependencies.database import get_db  # Get DB
+from dependencies.token import get_current_user  # Get current user
+from schemas.page import PageCreate, PageUpdate, PageRead  # Schemas
+from models import User, Page
+
+# Router
+router = APIRouter(
+    prefix="/pages",
+    tags=["Page"],
+)
+
+
+@router.get("", response_model=list[PageRead])
+async def list_pages(
+    user: User = Depends(get_current_user),
+    db: Session = Depends(get_db),
+):
+    db_pages = (
+        db.query(Page)
+        .where(
+            Page.user_id == user.id,
+        )
+        .all()
+    )
+
+    return db_pages
+
+
+@router.get("/{page_id}", response_model=PageRead)
+async def get_page(
+    page_id: UUID,
+    user: User = Depends(get_current_user),
+    db: Session = Depends(get_db),
+):
+    page = db.get(Page, page_id)
+
+    if not page:
+        raise HTTPException(
+            status_code=404,
+            detail="Page not found",
+        )
+
+    if page.user_id is not user.id:
+        raise HTTPException(
+            status_code=404,
+            detail="This page is not yours",
+        )
+
+    return page
+
+
+@router.post("", response_model=PageRead)
+async def create_page(
+    page_data: PageCreate,
+    user: User = Depends(get_current_user),
+    db: Session = Depends(get_db),
+):
+    db_page = Page(**page_data.model_dump(), user_id=user.id)
+
+    db.add(db_page)
+    db.commit()
+    db.refresh(db_page)
+
+    return db_page
+
+
+@router.patch("/{page_id}", response_model=PageRead)
+async def update_page(
+    page_id: UUID,
+    page_data: PageUpdate,
+    user: User = Depends(get_current_user),
+    db: Session = Depends(get_db),
+):
+    db_page = db.get(Page, page_id)
+
+    if not db_page:
+        raise HTTPException(
+            status_code=404,
+            detail="Page not found",
+        )
+
+    if db_page.user_id is not user.id:
+        raise HTTPException(
+            status_code=404,
+            detail="This page is not yours",
+        )
+
+    for key, value in page_data.model_dump(exclude_unset=True).items():
+        setattr(db_page, key, value)
+
+    db.commit()
+    db.refresh(db_page)
+
+    return db_page
+
+
+@router.delete("/{page_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_page(
+    page_id: UUID,
+    user: User = Depends(get_current_user),
+    db: Session = Depends(get_db),
+):
+    db_page = db.get(Page, page_id)
+
+    if not db_page:
+        raise HTTPException(
+            status_code=404,
+            detail="Page not found",
+        )
+
+    if db_page.user_id is not user.id:
+        raise HTTPException(
+            status_code=404,
+            detail="This page is not yours",
+        )
+
+    db_page.deleted_at = datetime.now(timezone.utc)
+
+    db.commit()
+    db.refresh(db_page)
+
+    return None
