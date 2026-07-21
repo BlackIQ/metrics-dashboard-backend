b85d9b7c045196292b6e91fe6e9fbda0bb6adb08
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2026-07-21 17:44:25 +0330

    Base of api key dependency

---
+++ dependencies/key.py
@@ -0,0 +1,24 @@
+# FastAPI
+from fastapi import status, Security, HTTPException
+from fastapi.security import APIKeyHeader
+
+# Application
+from core.settings import settings  # Settings
+
+# API Key Schema
+apikey_schema = APIKeyHeader(name="X-API-KEY", description="API Key in header")
+
+
+# API-KEY Dependency
+async def apikey(api_key: str = Security(apikey_schema)):
+    if not api_key:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Missing API key"
+        )
+
+    if api_key != settings.secret:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid API key"
+        )
+
+    return api_key
