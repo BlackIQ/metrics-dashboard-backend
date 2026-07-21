495f9b9525e4c376959e31778892a6954f667ba0
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2026-07-21 17:42:40 +0330

    Add algo and secret as a test

--- core/settings.py
+++ core/settings.py
@@ -5,6 +5,8 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 # Class Settings
 class Settings(BaseSettings):
     database_url: str = ""
+    secret: str = ""
+    algorithm: str = ""
 
     model_config = SettingsConfigDict(env_file=".env")
 
