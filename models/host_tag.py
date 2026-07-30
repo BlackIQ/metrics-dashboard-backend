1948ddcdd3457ca07b85487559951a953035655a
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2026-07-30 19:10:03 +0330

    The association table for host-tag

---
+++ models/host_tag.py
@@ -0,0 +1,34 @@
+# SQLAlchemy
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+# UUID
+import uuid
+
+# Application
+from base import BaseModel  # Base
+
+
+# Host - Tag Model
+class HostTag(BaseModel):
+    __tablename__ = "host_tags"
+
+    # Foreign Keys
+    host_id: Mapped[uuid.UUID] = mapped_column(
+        ForeignKey("hosts.id"),
+        primary_key=True,
+    )
+    tag_id: Mapped[uuid.UUID] = mapped_column(
+        ForeignKey("tags.id"),
+        primary_key=True,
+    )
+
+    # Relationships
+    host: Mapped["Host"] = relationship(
+        "Host",
+        back_populates="tags",
+    )
+    tag: Mapped["Tag"] = relationship(
+        "Tag",
+        back_populates="hosts",
+    )
