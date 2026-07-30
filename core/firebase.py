65ea1d2ed39265875f766ce7a86fbf4244aeaaea
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2026-07-31 02:18:56 +0330

    Loads from new settings

--- core/firebase.py
+++ core/firebase.py
@@ -5,8 +5,11 @@ from firebase_admin import credentials
 # Application
 from core.settings import settings
 
+# Load cerds
+cred_dict = settings.get_firebase_credentials_dict()
+
 # Firebase credentials
-cred = credentials.Certificate(settings.firebase_cerds)
+cred = credentials.Certificate(cred_dict)
 
 # Initialize Firebase Admin
 firebase_admin.initialize_app(cred)
