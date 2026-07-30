0d296b116ac22998a60bab56c3cc0d9a7a75e012
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2026-07-30 20:26:29 +0330

    Well, host schemas are done

--- schemas/host.py
+++ schemas/host.py
@@ -8,37 +8,65 @@ from pydantic.networks import IPvAnyAddress
 # Application
 from base import BaseSchema  # Base
 from enums import Communication  # Enums
+from schemas.tag import TagRead  # Schema for Tag
+from schemas.group import GroupRead  # Schema for Group
 
 
 # Host Schema
 class HostCreate(BaseSchema):
     name: str
     description: str
+
     ipv4: IPvAnyAddress
     dns: str
     port: int
+
     api_key: str
     communication: Communication
+
     is_active: bool
+
+    group_id: UUID
+    tag_ids: list[UUID]
 
 
 # Host profile
 class HostUpdate(BaseSchema):
     name: str | None = None
     description: str | None = None
+
     ipv4: IPvAnyAddress | None = None
     dns: str | None = None
     port: int | None = None
+
     api_key: str | None = None
     communication: Communication | None = None
+
     is_active: bool | None = None
 
+    group_id: UUID | None = None
+    tag_ids: list[UUID] | None = None
 
-# Host email
-class HostRead(HostCreate):
+
+# Host read
+class HostRead(BaseSchema):
     id: UUID
 
+    name: str
+    description: str
+
+    ipv4: IPvAnyAddress
+    dns: str
+    port: int
+
+    api_key: str
+    communication: Communication
+
+    is_active: bool
     agent_availability: bool
+
+    group: GroupRead
+    tags: list[TagRead]
 
     created_at: datetime
     updated_at: datetime
