522ffb2bf93597f36dd47dfca3fd776bd1624dd3
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2026-07-30 19:09:31 +0330

    Relation to host

--- models/group.py
+++ models/group.py
@@ -39,3 +39,7 @@ class Group(BaseModel):
         "User",
         back_populates="groups",
     )
+    hosts: Mapped["Host"] = relationship(
+        "Host",
+        back_populates="group",
+    )
