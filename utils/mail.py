6064fb59b4628918e613f1cdd033462db2083872
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2026-07-31 15:13:56 +0330

    Update send_email and list of emails. Security, Info and Support

--- utils/mail.py
+++ utils/mail.py
@@ -2,19 +2,28 @@
 import resend
 
 # Application
-from core.settings import settings
+from core.settings import settings  # Settings
 
-# Config Resend
+# Setup Resend
 resend.api_key = settings.resend_apikey
 
 
-# Send email
+# Emails (Senders)
+class MailSender:
+    INFO = "OpenHubble Info <info@openhubble.com>"
+    SECURITY = "OpenHubble Security <security@openhubble.com>"
+    SUPPORT = "OpenHubble Support <support@openhubble.com>"
+
+
 def send_email(sender: str, to: str, subject: str, content: str):
-    resend.Emails.send(
-        {
-            "from": sender,
-            "to": to,
-            "subject": subject,
-            "html": content,
-        }
-    )
+    try:
+        resend.Emails.send(
+            {
+                "from": sender,
+                "to": to,
+                "subject": subject,
+                "html": content,
+            }
+        )
+    except Exception as e:
+        print(f"[MAIL ERROR] Failed to send email to {to}: {e}")
