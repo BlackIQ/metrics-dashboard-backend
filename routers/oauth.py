3d0b0dfa0024fdd72431aa6dd9f6b716f660d7ff
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2026-07-31 18:42:34 +0330

    A little update in oauth routers

--- routers/oauth.py
+++ routers/oauth.py
@@ -28,12 +28,12 @@ router = APIRouter(
 )
 
 
-@router.post("/google", response_model=TokenSchema)
-async def google_login(
+async def process_oauth_login(
+    provider: str,
     payload: OAuthSchema,
     background_tasks: BackgroundTasks,
-    db: Session = Depends(get_db),
-):
+    db: Session,
+) -> TokenSchema:
     try:
         decoded_token = firebase_auth.verify_id_token(payload.id_token)
     except Exception:
@@ -49,7 +49,7 @@ async def google_login(
     if not email:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Email not provided by Google account",
+            detail=f"Email not provided by {provider.capitalize()} account",
         )
 
     name_parts = name.split(" ") if name else ["", ""]
@@ -62,7 +62,7 @@ async def google_login(
             or_(
                 and_(
                     User.oauth_id == uid,
-                    User.oauth_provider == "google",
+                    User.oauth_provider == provider,
                 ),
                 User.email == email,
             )
@@ -81,19 +81,17 @@ async def google_login(
             last_name=last_name,
             is_confirmed=True,
             is_active=True,
-            oauth_provider="google",
+            oauth_provider=provider,
             oauth_id=uid,
         )
-
         db.add(user)
         db.commit()
         db.refresh(user)
 
     elif not user.oauth_provider:
-        user.oauth_provider = "google"
+        user.oauth_provider = provider
         user.oauth_id = uid
         user.is_confirmed = True
-
         db.commit()
         db.refresh(user)
 
@@ -103,6 +101,7 @@ async def google_login(
             detail="Account is inactive",
         )
 
+    # 4. Background Emails
     if is_new_user:
         background_tasks.add_task(
             send_email,
@@ -126,203 +125,30 @@ async def google_login(
         access_token=access_token,
         token_type="bearer",
     )
+
+
+@router.post("/google", response_model=TokenSchema)
+async def google_authentication(
+    payload: OAuthSchema,
+    background_tasks: BackgroundTasks,
+    db: Session = Depends(get_db),
+):
+    return await process_oauth_login("google", payload, background_tasks, db)
 
 
 @router.post("/facebook", response_model=TokenSchema)
-async def facebook_login(
+async def facebook_authentication(
     payload: OAuthSchema,
     background_tasks: BackgroundTasks,
     db: Session = Depends(get_db),
 ):
-    try:
-        decoded_token = firebase_auth.verify_id_token(payload.id_token)
-    except Exception:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Invalid or expired Firebase ID token",
-        )
-
-    uid = decoded_token.get("uid")
-    email = decoded_token.get("email")
-    name = decoded_token.get("name", "")
-
-    if not email:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Email not provided by Facebook account",
-        )
-
-    name_parts = name.split(" ") if name else ["", ""]
-    first_name = name_parts[0]
-    last_name = " ".join(name_parts[1:]) if len(name_parts) > 1 else ""
-
-    user = (
-        db.query(User)
-        .filter(
-            or_(
-                and_(
-                    User.oauth_id == uid,
-                    User.oauth_provider == "facebook",
-                ),
-                User.email == email,
-            )
-        )
-        .first()
-    )
-
-    is_new_user = False
-
-    if not user:
-        is_new_user = True
-        user = User(
-            email=email,
-            password=None,
-            first_name=first_name,
-            last_name=last_name,
-            is_confirmed=True,
-            is_active=True,
-            oauth_provider="facebook",
-            oauth_id=uid,
-        )
-
-        db.add(user)
-        db.commit()
-        db.refresh(user)
-
-    elif not user.oauth_provider:
-        user.oauth_provider = "facebook"
-        user.oauth_id = uid
-        user.is_confirmed = True
-
-        db.commit()
-        db.refresh(user)
-
-    if not user.is_active:
-        raise HTTPException(
-            status_code=status.HTTP_403_FORBIDDEN,
-            detail="Account is inactive",
-        )
-
-    if is_new_user:
-        background_tasks.add_task(
-            send_email,
-            sender=MailSender.INFO,
-            to=user.email,
-            subject="Welcome to OpenHubble!",
-            content=get_welcome_email(),
-        )
-    else:
-        background_tasks.add_task(
-            send_email,
-            sender=MailSender.SECURITY,
-            to=user.email,
-            subject="Security Alert: New Sign-in to OpenHubble",
-            content=get_signin_notification_email(),
-        )
-
-    access_token = create_token(user.id)
-
-    return TokenSchema(
-        access_token=access_token,
-        token_type="bearer",
-    )
+    return await process_oauth_login("facebook", payload, background_tasks, db)
 
 
 @router.post("/github", response_model=TokenSchema)
-async def github_login(
+async def github_authentication(
     payload: OAuthSchema,
     background_tasks: BackgroundTasks,
     db: Session = Depends(get_db),
 ):
-    try:
-        decoded_token = firebase_auth.verify_id_token(payload.id_token)
-    except Exception:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Invalid or expired Firebase ID token",
-        )
-
-    uid = decoded_token.get("uid")
-    email = decoded_token.get("email")
-    name = decoded_token.get("name", "")
-
-    if not email:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Email not provided by GitHub account",
-        )
-
-    name_parts = name.split(" ") if name else ["", ""]
-    first_name = name_parts[0]
-    last_name = " ".join(name_parts[1:]) if len(name_parts) > 1 else ""
-
-    user = (
-        db.query(User)
-        .filter(
-            or_(
-                and_(
-                    User.oauth_id == uid,
-                    User.oauth_provider == "github",
-                ),
-                User.email == email,
-            )
-        )
-        .first()
-    )
-
-    is_new_user = False
-
-    if not user:
-        is_new_user = True
-        user = User(
-            email=email,
-            password=None,
-            first_name=first_name,
-            last_name=last_name,
-            is_confirmed=True,
-            is_active=True,
-            oauth_provider="github",
-            oauth_id=uid,
-        )
-
-        db.add(user)
-        db.commit()
-        db.refresh(user)
-
-    elif not user.oauth_provider:
-        user.oauth_provider = "github"
-        user.oauth_id = uid
-        user.is_confirmed = True
-
-        db.commit()
-        db.refresh(user)
-
-    if not user.is_active:
-        raise HTTPException(
-            status_code=status.HTTP_403_FORBIDDEN,
-            detail="Account is inactive",
-        )
-
-    if is_new_user:
-        background_tasks.add_task(
-            send_email,
-            sender=MailSender.INFO,
-            to=user.email,
-            subject="Welcome to OpenHubble!",
-            content=get_welcome_email(),
-        )
-    else:
-        background_tasks.add_task(
-            send_email,
-            sender=MailSender.SECURITY,
-            to=user.email,
-            subject="Security Alert: New Sign-in to OpenHubble",
-            content=get_signin_notification_email(),
-        )
-
-    access_token = create_token(user.id)
-
-    return TokenSchema(
-        access_token=access_token,
-        token_type="bearer",
-    )
+    return await process_oauth_login("github", payload, background_tasks, db)
