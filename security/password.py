b7d2eb170102dc955ea3637bb2dd1cc63408a9e7
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2026-08-01 13:02:07 +0330

    Password validator

--- security/password.py
+++ security/password.py
@@ -2,7 +2,7 @@
 from pwdlib import PasswordHash
 
 # Regex
-import re  # TODO: Validate password
+import re
 
 # Hash instance
 password_hash = PasswordHash.recommended()
@@ -18,17 +18,23 @@ def verify_password(password: str, hashed: str) -> bool:
     return password_hash.verify(password, hashed)
 
 
-# def validate_password_strength(password: str) -> bool:
-#     if not isinstance(password, str):
-#         return False
-#     if len(password) < 12:
-#         return False
-#     if not re.search(r"[a-z]", password):
-#         return False
-#     if not re.search(r"[A-Z]", password):
-#         return False
-#     if not re.search(r"\d", password):
-#         return False
-#     if not re.search(r"[^A-Za-z0-9]", password):
-#         return False
-#     return True
+def validate_password_strength(password: str) -> bool:
+    if not isinstance(password, str):
+        return False
+
+    if len(password) < 12:
+        return False
+
+    if not re.search(r"[a-z]", password):
+        return False
+
+    if not re.search(r"[A-Z]", password):
+        return False
+
+    if not re.search(r"\d", password):
+        return False
+
+    if not re.search(r"[^A-Za-z0-9]", password):
+        return False
+
+    return True
